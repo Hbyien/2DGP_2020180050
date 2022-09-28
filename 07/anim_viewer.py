a076812b7b7716e8da64258df90c472c94ff7db6
Hbyien <sasg6780@naver.com>
2022-09-28 19:48:19 +0900

    drill#7
    2020180050 이혜빈

---
+++ 07/anim_viewer.py
@@ -0,0 +1,27 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('anim_viewer.png')
+
+x = 0
+frame = 0
+bottom = 0
+
+while (x < 800):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame*97, 280-bottom, 97, 70, x, 90)
+    update_canvas()
+    frame = (frame+1)%6
+    if frame ==0:
+        bottom=bottom+70
+        if bottom > 280:
+            bottom=0
+            delay(0.5)
+    x += 5
+    delay(0.05)
+    get_events()
+
+
+close_canvas()
+
